8d6f9061db92ff6a212cbbd109522303e27d00ff
Santiago Saavedra <santiagosaavedra@gmail.com>
2015-10-14 19:05:10 +0200

    GH-pages lonely commit at

---
+++ slide_config.js
@@ -0,0 +1,26 @@
+var SLIDE_CONFIG = {
+   // Slide Settings
+   settings: {
+     title: 'Plan de dominación mundial', 
+     subtitle: 'Unha idea para incrementar a actividade de GPUL', 
+     useBuilds: true, // Default: true. False will turn off slide animation builds. 
+     usePrettify: true, // Default: true 
+     enableSlideAreas: true, // Default: true. False turns off the click areas on either slide of the slides.
+     enableTouch: true, // Default: true. If touch support should enabled. Note: the device must support touch.
+     favIcon: 'images/gpul-nuevo.svg', 
+     fonts: [
+       'Open Sans:regular,semibold,italic,italicsemibold',
+       'Source Code Pro'
+     ],
+   }, 
+ 
+   // Author information
+   presenters: [{
+     name: 'Santiago Saavedra López', 
+     company: 'Grupo de Programadores e Usuarios de Linux', 
+     gplus: '', 
+     twitter: 'gpul_', 
+     www: 'https://gpul.org', 
+     github: 'https://github.com/gpul-org', 
+   }]
+};
